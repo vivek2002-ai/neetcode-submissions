132e3da96bba782dfc7f029a6d26c14384d7fa30
vivek2002-ai <62781405+vivek2002-ai@users.noreply.github.com>
2026-06-25 21:35:38 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        my_map = dict()
+        for x in s:
+            if x in my_map.keys():
+                my_map[x] += 1
+            else:
+                my_map[x] = 1
+        for y in t:
+            if y in my_map.keys():
+                my_map[y] -= 1
+            else:
+                return False
+        for v in my_map.values():
+            if v != 0:
+                return False
+        return True
+        
